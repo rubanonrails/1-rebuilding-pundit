185fdd1fd5605c50ca4f667a631ea5f68b2a7ddd
Alexandre Ruban <alexandre@hey.com>
2023-05-27 11:27:33 +0200

    Create the User model with the Devise gem
    In this commit, we create a User model with Devise. The User model also
    has a role attribute, which is an enum. The User can be either a
    reader or an admin.

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+# Flexible authentication solution for Rails with Warden.
+gem "devise"
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Deleting old data..."
+User.destroy_all
+
+puts "Creating new data..."
+reader = User.create! email: "reader@email.com", password: "password", role: "reader"
+admin = User.create! email: "admin@email.com", password: "password", role: "admin"
+
+puts "Seeding complete!"
